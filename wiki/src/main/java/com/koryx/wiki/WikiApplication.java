d7132399e5f69a606dd08859fcf345e2a9a3c2e3
Koryx from Ny'alotha <65605032+Koryx97@users.noreply.github.com>
2022-08-11 23:06:36 -0400

    Add files via upload

---
+++ wiki/src/main/java/com/koryx/wiki/WikiApplication.java
@@ -0,0 +1,27 @@
+package com.koryx.wiki;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class WikiApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("lauched successfully!!");
+		LOG.info("address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
