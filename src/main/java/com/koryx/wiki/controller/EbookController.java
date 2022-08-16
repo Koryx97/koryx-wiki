b344085aec068d1284a769129470c44d01149a27
Koryx97 <koryxchan@outlook.com>
2022-08-16 12:59:00 +0800

    3-7 encapsulate request and response arguments

--- src/main/java/com/koryx/wiki/controller/EbookController.java
+++ src/main/java/com/koryx/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.koryx.wiki.controller;
 
-import com.koryx.wiki.domain.Ebook;
+import com.koryx.wiki.req.EbookReq;
 import com.koryx.wiki.resp.CommonResp;
+import com.koryx.wiki.resp.EbookResp;
 import com.koryx.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/ebook/list")
-    public CommonResp list() {
+    public CommonResp list(EbookReq req) {
 
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
